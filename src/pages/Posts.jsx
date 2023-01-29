8fb43ffa19eb0f9f0240f91e4722837953e42a44
mifouts <fouts.mitch15@gmail.com>
2023-01-29 16:07:50 -0500

    fetching data

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Posts() {
-  return (
-    <div>
-      <h1>posts page</h1>
-    </div>
-  );
+  const { id } = useParams();
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      console.log(data);
+    }
+    fetchPosts();
+  }, []);
+  return <div>{id}</div>;
 }
