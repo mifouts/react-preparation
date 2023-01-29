f13e4f69b1f178d0ecaf854e6894de7b9b1da873
mifouts <fouts.mitch15@gmail.com>
2023-01-29 15:53:16 -0500

    creating and importing post component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Posts from "./pages/Posts";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path=":id"></Route>
+          <Route path=":id" element={<Posts />}></Route>
         </Routes>
       </div>
       ;
