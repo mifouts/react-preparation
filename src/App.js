c1743bd9abe84e54d1eb954ce21e2ca4dfd09aea
mifouts <fouts.mitch15@gmail.com>
2023-01-29 15:52:12 -0500

    creating home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route></Route>
-          <Route></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path=":id"></Route>
         </Routes>
       </div>
       ;
