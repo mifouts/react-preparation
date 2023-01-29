ba377db85de255912b796ca3c26884714d48927c
mifouts <fouts.mitch15@gmail.com>
2023-01-29 16:11:14 -0500

    installing and using axios instead of fetch

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function Posts() {
   const { id } = useParams();
   useEffect(() => {
     async function fetchPosts() {
-      const data = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/1"
+      );
       console.log(data);
     }
     fetchPosts();
