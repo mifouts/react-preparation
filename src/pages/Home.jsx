f48d5ed505066655192c7ad7bdec4e070f44615b
mifouts <fouts.mitch15@gmail.com>
2023-01-30 22:43:25 -0500

    calling users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,28 +13,32 @@ export default function Home() {
   useEffect(() => fetchUsers(), []);
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="user-list">
-          <div className="user">
-            <div className="user-card">
-              <div className="user-card__container">
-                <h3>{user.name}</h3>
-                <p>
-                  <b>Email:</b> {user.email}
-                </p>
-                <p>
-                  <b>Phone:</b> {user.phone}
-                </p>
-                <p>
-                  <b>Website:</b>
-                  {user.website}
-                </p>
+    <>
+      {user.map((user) => (
+        <div className="container">
+          <div className="row">
+            <div className="user-list">
+              <div className="user">
+                <div className="user-card">
+                  <div className="user-card__container">
+                    <h3>{user.name}</h3>
+                    <p>
+                      <b>Email:</b> {user.email}
+                    </p>
+                    <p>
+                      <b>Phone:</b> {user.phone}
+                    </p>
+                    <p>
+                      <b>Website:</b>
+                      {user.website}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
