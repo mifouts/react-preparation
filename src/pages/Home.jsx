37b85aa53f9703b6bc9a098f55027b3a9dd789b4
mifouts <fouts.mitch15@gmail.com>
2023-01-30 23:14:55 -0500

    using navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Home = () => {
-  let navigate = useNavigate();
+  let Navigate = useNavigate();
   const [users, setUsers] = useState([]);
   async function fetchUsers() {
     const { data } = await axios.get(
@@ -22,7 +22,7 @@ const Home = () => {
         <div className="user-list">
           {users.map((user) => (
             <div className="user" key={user.id}>
-              <div className="user-card" onClick={navigate()}>
+              <div className="user-card" onClick={() => Navigate()}>
                 <div className="user-card__container">
                   <h3>{user.name}</h3>
                   <p>
