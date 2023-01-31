6e124e1d2ff48066a13f425605d4500d605f77fb
mifouts <fouts.mitch15@gmail.com>
2023-01-30 22:25:33 -0500

    styling home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>this is home page</h1>
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          <div className="user">
+            <div className="user-card">
+              <div className="user-card__container">
+                <h3>{user.name}</h3>
+                <p>
+                  <b>Email:</b> {user.email}
+                </p>
+                <p>
+                  <b>Phone:</b> {user.phone}
+                </p>
+                <p>
+                  <b>Website:</b>
+                  {user.website}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
