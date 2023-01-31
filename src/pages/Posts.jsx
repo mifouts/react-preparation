6300879a08d0208489e51abbf5ad21ac1a3fc7a7
mifouts <fouts.mitch15@gmail.com>
2023-01-30 21:02:41 -0500

    implementing a loading state

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -5,6 +5,11 @@ import axios from "axios";
 export default function Posts() {
   const { id } = useParams();
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  function onSearch() {
+    console.log("onSearch");
+  }
 
   useEffect(() => {
     async function fetchPosts() {
@@ -12,6 +17,7 @@ export default function Posts() {
         `https://jsonplaceholder.typicode.com/posts?userId=${id}`
       );
       setPosts(data);
+      setLoading(false);
     }
     fetchPosts();
   }, []);
@@ -22,23 +28,26 @@ export default function Posts() {
         <div className="post__search--container">
           <label className="post__search--label">Search by Id</label>
           <input type="number" />
-          <button>Enter</button>
+          <button onClick={() => onSearch()}>Enter</button>
         </div>
       </div>
-      {posts.map((post) => (
-        <div className="post">
-          <div className="post__title">{post.title}</div>
-          <p className="post__body">{post.body}</p>
-        </div>
-      ))}
-      <div className="post">
-        <div className="post__title">
-          <div className="post__title--skeleton"></div>
-        </div>
-        <div className="post__body">
-          <p className="post__body--skeleton"></p>
-        </div>
-      </div>
+      {loading
+        ? new Array(10).fill(0).map((_, index) => (
+            <div className="post" key={index}>
+              <div className="post__title">
+                <div className="post__title--skeleton"></div>
+              </div>
+              <div className="post__body">
+                <p className="post__body--skeleton"></p>
+              </div>
+            </div>
+          ))
+        : posts.map((post) => (
+            <div className="post">
+              <div className="post__title">{post.title}</div>
+              <p className="post__body">{post.body}</p>
+            </div>
+          ))}
     </>
   );
 }
