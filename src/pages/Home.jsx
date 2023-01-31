1ad02cea95456fc30fd654d93d79a6376dd687b0
mifouts <fouts.mitch15@gmail.com>
2023-01-30 22:56:19 -0500

    calling users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,33 +15,31 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      <div className="container">
-        <div className="row">
-          <div className="user-list">
-            {users.map((user) => (
-              <div className="user" key={user.id}>
-                <div className="user-card">
-                  <div className="user-card__container">
-                    <h3>{user.name}</h3>
-                    <p>
-                      <b>Email:</b> {user.email}
-                    </p>
-                    <p>
-                      <b>Phone:</b> {user.phone}
-                    </p>
-                    <p>
-                      <b>Website:</b>
-                      {user.website}
-                    </p>
-                  </div>
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          {users.map((user) => (
+            <div className="user" key={user.id}>
+              <div className="user-card">
+                <div className="user-card__container">
+                  <h3>{user.name}</h3>
+                  <p>
+                    <b>Email:</b> {user.email}
+                  </p>
+                  <p>
+                    <b>Phone:</b> {user.phone}
+                  </p>
+                  <p>
+                    <b>Website:</b>
+                    {user.website}
+                  </p>
                 </div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
